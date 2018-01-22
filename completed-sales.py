d30dd476454916b9986547a9865118f4d0c354da
ym26 <ym26@nyu.edu>
2018-01-22 18:55:27 -0500

    Transfered show ebay results into sub routine. Calculated number of entries per page. Make multiple calls to ebay to retrieve each page with multiple entries

--- completed-sales.py
+++ completed-sales.py
@@ -15,6 +15,54 @@ from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 import datetime
 import time
+import sys
+import pprint
+
+
+
+
+
+#################################################
+#
+# Run Finding API for Ebay to search for 
+# completed items that were sold
+#
+def runAPI(page, numEntriesPerPage, minusDays, nowTime):
+
+
+  ### Reference config file to get APPID
+  api = Finding(config_file='config/ebay.yaml')
+
+
+
+  # Setup api_request options to later pass to api execution
+  api_request = {
+    'categoryId': '15687',
+    'itemFilter': [
+      {'name': 'MinPrice',      'value': '30'},          # Minimum Price
+      {'name': 'MaxPrice',      'value': '1000'},        # Maximum Price
+      {'name': 'SoldItemsOnly', 'value': 'True'},        # Show only successfully sold items
+      {'name': 'LocatedIn',     'value': 'US'},          # Located in United States
+      {'name': 'StartTimeFrom', 'value': minusDays},     # Time in UTC format YYYY-MM-DDTHH:MM:SS.000Z (Z for Zulu Time). (e.g: '2018-01-1T08:00:01')
+      {'name': 'EndTimeTo',     'value': nowTime}        # Time in UTC format YYYY-MM-DDTHH:MM:SS.000Z (Z for Zulu Time). (e.g: '2018-01-19T14:30:01')
+    ],
+    'paginationInput': {
+      'entriesPerPage': numEntriesPerPage,
+      'pageNumber': page
+      #'pageNumber': '1'                                 # e.g: custom page
+    }
+  } 
+
+
+  ### Execute against findCompletedItems with designated api_request options
+  api.execute('findCompletedItems', api_request)
+
+  ### Return results as json form dictionary
+  results = api.response.dict()
+    
+  return results
+
+
 
 
 
@@ -25,62 +73,14 @@ import time
 #
 def CalculateDate(days):
 
-  # Convert current time to epoch time
+  ### Convert current time to epoch time
   epoch = int(time.time())
   
-  # Subtract x days from current epoch time, (e.g: seven days = 60 * 60 * 24 * 7 = 604800)
+  ### Subtract x days from current epoch time, (e.g: seven days = 60 * 60 * 24 * 7 = 604800)
   minusDays = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(epoch - (60 * 60 * 24 * days)))
   
   return minusDays
 
-
-
-
-#################################################
-#
-# Run Finding API for Ebay to search for 
-# completed items that were sold
-#
-def runAPI():
-  # Reference config file to get APPID
-  api = Finding(config_file='config/ebay.yaml')
-
-
-  page = 1    # range can be from 1 to 100, but you will need to loop over each page if greater than one
-  days = 5    # number of days to subtract from today 
-
-  minusDays = CalculateDate(days)                              # date and time today minus number of days
-  nowTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())  # date and time today
-
-  
-
-  # Setup api_request options to later pass to api execution
-  api_request = {
-    'categoryId': '15687',
-    'itemFilter': [
-      {'name': 'MinPrice', 'value': '30'},               # Minimum Price
-      {'name': 'MaxPrice', 'value': '1000'},             # Maximum Price
-      {'name': 'SoldItemsOnly', 'value': 'True'},        # Show only successfully sold items
-      {'name': 'LocatedIn', 'value': 'US'},              # Located in United States
-      {'name': 'StartTimeFrom', 'value': minusDays},     # Time in UTC format YYYY-MM-DDTHH:MM:SS.000Z (Z for Zulu Time). (e.g: '2018-01-1T08:00:01')
-      {'name': 'EndTimeTo', 'value': nowTime}            # Time in UTC format YYYY-MM-DDTHH:MM:SS.000Z (Z for Zulu Time). (e.g: '2018-01-19T14:30:01')
-    ],
-    'paginationInput': {
-      'entriesPerPage': '100',
-      'pageNumber': page
-      #'pageNumber': '1'
-    }
-  } 
-
-
-
-  # Execute against findCompletedItems with designated api_request options
-  api.execute('findCompletedItems', api_request)
-
-  # Return results as json form dictionary
-  results = api.response.dict()
-  
-  return results
 
 
 
@@ -93,7 +93,7 @@ def runAPI():
 def WriteToFile(categoryName, categoryId, itemId, currencyValue, viewItemURL):
 
 
-  # Write to file
+  ### Write to file
   with open('listings.txt', 'a') as f:
     f.write("{0}, {1}, {2}, {3:5}, {4}\n".format(categoryName, categoryId, itemId, currencyValue, viewItemURL))
 
@@ -101,27 +101,42 @@ def WriteToFile(categoryName, categoryId, itemId, currencyValue, viewItemURL):
 
 
 
-def main():
- 
-  results = runAPI()
 
-  # Empty out the file
-  open('listings.txt', 'w').close()
- 
- 
-  
-  # HEADING: Display total pages, entries, number of pages and entries per page
+
+
+
+
+#################################################
+#
+# Get Header info
+#
+def getHEADER(results):
+
+  ### HEADING: Display total pages, entries, number of pages and entries per page
   try:
     print "Ack: %s       " % results['ack']
     print "Timestamp: %s " % results['timestamp']
     print "Version: %s   " % results['version']
     print
-    print "-"*150
+    #print "-"*150
   except KeyError, e:
     print "[-] %s" % e
 
 
 
+
+
+
+
+
+
+ 
+#################################################
+#
+# Show results from ebay
+#
+def showResults(results): 
+  
 
   """
    - Show me only those items that have been sold
@@ -131,7 +146,7 @@ def main():
   try:
     for i in range(len(results['searchResult']['item'])):
 
-      # Test watchcount for KeyError (its when value of key is not present). If so assign default value of zero
+      ### Test watchcount for KeyError (its when value of key is not present). If so assign default value of zero
       try:
         watchCount = results['searchResult']['item'][i]['listingInfo']['watchCount']
       except KeyError:
@@ -139,8 +154,7 @@ def main():
 
 
 
-      ''' Definitions 
-      '''
+      ### Definitions 
       itemId               = results['searchResult']['item'][i]['itemId']
       categoryId           = results['searchResult']['item'][i]['primaryCategory']['categoryId']
       categoryName         = results['searchResult']['item'][i]['primaryCategory']['categoryName']
@@ -158,12 +172,12 @@ def main():
 
 
 
-      # Print Category ID and Name
+      ### Print Category ID and Name
       print "{0:3}) CategoryID: {1}, Category Name: {2}".format(i, categoryId, categoryName)
                                       
                                      
 
-      # Print selected items from finding api
+      ### Print selected items from finding api
       print "{0:3}) Top Rated: {1:5}, Market: {2:7}, Currency: {3:3}, Price: {4}, Selling State: {5}, Listing: {6}, WatchCount: {7}".format(i, \
                                       topRatedListing, \
                                       globalId, \
@@ -176,7 +190,7 @@ def main():
       print "{0:3}) Condition:  {1}".format(i, conditionDisplayName)
 
 
-      # Extract Date and Time. Use replace() function to replace elements with spaces. Then grab first two elements date and time
+      ### Extract Date and Time. Use replace() function to replace elements with spaces. Then grab first two elements date and time
       (startDate, startTime) = (startTime).replace('T', ' ').replace('.', ' ').split()[0:2]
       (endDate, endTime)     =   (endTime).replace('T', ' ').replace('.', ' ').split()[0:2]
 
@@ -184,13 +198,13 @@ def main():
       print "{0:3}) End Time:   {1} {2}".format(i, endDate, endTime)
 
 
-      # URL
+      ### URL
       print "{0:3}) {1}".format(i, viewItemURL)
       print "-"*150
 
 
 
-      # Write to File
+      ### Save data to file
       WriteToFile(categoryName, categoryId, itemId, currencyValue, viewItemURL)
 
 
@@ -198,6 +212,85 @@ def main():
     print "No Search Results Found"
     print "Try to adjust itemFilter options"
     print e
+
+
+
+
+def main():
+
+
+  ### Create Empty File
+  open('listings.txt', 'w').close()
+
+
+
+  ### The page variable is used to retreive page number
+  ### The days variable is used to deduct number of days from today
+  page = 1
+  days = 2
+
+
+  ### Calculate From Date the time from which to search
+  minusDays = CalculateDate(days)                              
+
+
+  ### Calculate Todays Date and Time or time to which to search
+  nowTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())  
+
+
+  ### Qeury api to understand how many pages exist by sampling page header
+  numEntriesPerPage = 100
+  results = runAPI(page, numEntriesPerPage, minusDays, nowTime)
+
+
+
+  totalPages   = int(results['paginationOutput']['totalPages'])      # e.g: (6 pages)
+  totalEntries = int(results['paginationOutput']['totalEntries'])    # e.g: (515 entries)
+
+
+  print "totalPages:   {0}".format(totalPages)
+  print "totalEntries: {0}".format(totalEntries)
+
+
+  ### Get header information (e.g: ack, version, timestamp) 
+  getHEADER(results)
+
+
+
+  ### Now loop through total number of pages and get all sold items per page
+  for page in range(1, totalPages+1):
+
+    ### Get API Results
+    results = runAPI(page, numEntriesPerPage, minusDays, nowTime)
+
+ 
+    ### Get Page Number 
+    pageNumber = results['paginationOutput']['pageNumber']
+    print "Page Number: {0} Total Entries: {1} \n".format(pageNumber, totalEntries) 
+
+
+
+    ### Show results from Ebay 
+    showResults(results)
+
+
+
+    ### Calculate how many entries are left to fit on one page
+    totalEntries = totalEntries - numEntriesPerPage
+
+
+
+    '''
+    print "{0}, {1}".format(results['searchResult']['item'][0]['itemId'], results['searchResult']['item'][0]['viewItemURL'])
+    results = runAPI(page=2, days=5, numEntriesPerPage=100)
+    print "{0}, {1}".format(results['searchResult']['item'][0]['itemId'], results['searchResult']['item'][0]['viewItemURL'])
+
+    pp = pprint.PrettyPrinter(indent=1)
+    pp.pprint(results)
+    '''
+
+
+
 
 
 
